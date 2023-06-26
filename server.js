5de74f48afad8e1b9233ad0428001b0ff2b1f958
Andrew Carroll <andrewc411@yahoo.com>
2023-06-26 10:38:50 -0400

    updated files by moving them to project folder

--- server.js
+++ server.js
@@ -1,3 +1,27 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
+
+const app = express();
+
+const storage = multer.diskStorage({
+    destination: './uploads/',
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    }
+  });
+  const upload = multer({ storage: storage });
+
+  app.post('/upload', upload.single('photo'), (req, res) => {
+    res.send('Photo uploaded successfully!');
+  });
+
+  app.get('/photos/:filename', (req, res) => {
+    const filename = req.params.filename;
+    res.sendFile(path.join(__dirname, 'uploads', filename));
+  });
+
+  const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
